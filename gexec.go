609c52c16d752be43ef725bfe534b5b70cb3e246
masshash <42906988+masshash@users.noreply.github.com>
2023-03-09 23:21:14 +0900

    Rename receiver names

--- gexec.go
+++ gexec.go
@@ -11,25 +11,25 @@ type GroupedCmd struct {
 	jobObject *jobObject
 }
 
-func (gc *GroupedCmd) Start() error {
-	return gc.start()
-}
-
-func (gc *GroupedCmd) Run() error {
-	if err := gc.Start(); err != nil {
-		return err
-	}
-	return gc.Wait()
-}
-
-func (gc *GroupedCmd) SignalAll(sig os.Signal) error {
-	return gc.signalAll(sig)
-}
-
-func (gc *GroupedCmd) KillAll() error {
-	return gc.SignalAll(os.Kill)
-}
-
 func Grouped(cmd *exec.Cmd) *GroupedCmd {
 	return &GroupedCmd{Cmd: cmd}
+}
+
+func (c *GroupedCmd) Start() error {
+	return c.start()
+}
+
+func (c *GroupedCmd) Run() error {
+	if err := c.Start(); err != nil {
+		return err
+	}
+	return c.Wait()
+}
+
+func (c *GroupedCmd) SignalAll(sig os.Signal) error {
+	return c.signalAll(sig)
+}
+
+func (c *GroupedCmd) KillAll() error {
+	return c.SignalAll(os.Kill)
 }
--- gexec_linux.go
+++ gexec_linux.go
@@ -9,30 +9,30 @@ import (
 	"syscall"
 )
 
-func (gc *GroupedCmd) start() error {
-	if gc.Cmd.SysProcAttr != nil {
-		gc.Cmd.SysProcAttr.Setpgid = true
+func (c *GroupedCmd) start() error {
+	if c.Cmd.SysProcAttr != nil {
+		c.Cmd.SysProcAttr.Setpgid = true
 	} else {
-		gc.Cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
+		c.Cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
 	}
 
-	if err := gc.Cmd.Start(); err != nil {
+	if err := c.Cmd.Start(); err != nil {
 		return err
 	}
-	gc.pgid = gc.Cmd.Process.Pid
+	c.pgid = c.Cmd.Process.Pid
 	return nil
 }
 
-func (gc *GroupedCmd) signalAll(sig os.Signal) error {
-	if gc.pgid == -1 {
+func (c *GroupedCmd) signalAll(sig os.Signal) error {
+	if c.pgid == -1 {
 		return errors.New("invalid process group id")
 	}
-	if gc.pgid == 0 {
+	if c.pgid == 0 {
 		return errors.New("process group not initialized")
 	}
 	s, ok := sig.(syscall.Signal)
 	if !ok {
 		return errors.New("unsupported signal type")
 	}
-	return syscall.Kill(-gc.pgid, s)
+	return syscall.Kill(-c.pgid, s)
 }
